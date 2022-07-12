1cf11a5993f1e77e64711e742d525915a6aa6222
Marco Godoy <marcogodoyvalbuena@gmail.com>
2022-07-12 12:24:34 -0300

    WIP
    WIP

--- app.py
+++ app.py
@@ -1,71 +1,111 @@
-from math import prod
 from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-from products import products
+USER = os.getenv("DB_USER")
+PASSWORD = os.getenv("DB_PASSWORD")
+HOST = os.getenv("DB_HOST")
 
+app.config["MYSQL_HOST"] = HOST
+app.config["MYSQL_USER"] = USER
+app.config["MYSQL_PASSWORD"] = PASSWORD
+app.config["MYSQL_DB"] = "sql10505778"
 
-@app.route("/products", methods=["GET"])
-def getProducts():
-    return jsonify({"message": "Product's list", "products": products})
-
-
-@app.route("/products/<string:product_name>", methods=["GET"])
-def getProduct(product_name):
-    productFound = [product for product in products if product["name"] == product_name]
-    if len(productFound) > 0:
-        print(productFound)
-        return jsonify({"message": "Product found", "product": productFound})
-    else:
-        return {"message": "Product not found"}
+mysql = MySQL(app)
 
 
-@app.route("/products", methods=["POST"])
-def addProduct():
-    new_product = {
-        "name": request.json["name"],
-        "price": request.json["price"],
-        "quantity": request.json["quantity"],
-    }
-    products.append(new_product)
-    return jsonify({"message": "Product Added to List", "product": products})
-
-
-@app.route("/products/<string:product_name>", methods=["PUT"])
-def editProduct(product_name):
-    productFound = [product for product in products if product["name"] == product_name]
-    if len(productFound) > 0:
-        if request.json["name"] != "":
-            productFound[0]["name"] = request.json["name"]
-
-        if request.json["price"] != "" and request.json["price"] >= 0:
-            productFound[0]["price"] = request.json["price"]
-
-        if request.json["quantity"] > -1:
-            productFound[0]["quantity"] = request.json["quantity"]
-
-        return jsonify(
-            [{"message": "Product Updated", "Product": productFound}, products]
-        )
-    else:
-        return jsonify({"message": "Product not found"})
-
-
-@app.route("/products/<string:product_name>", methods=["DELETE"])
-def deleteProduct(product_name):
-    productFound = [product for product in products if product["name"] == product_name]
-
-    if len(productFound) > 0:
-        products.remove(productFound[0])
-        return jsonify(
-            {
-                "message": "Product Removed " + productFound[0]["name"],
-                "products": products,
+@app.route("/api/tasks", methods=["GET", "POST"])
+def getTasks():
+    if request.method == "GET":
+        cur = mysql.connection.cursor()
+        cur.execute("Select * from tasks")
+        results = cur.fetchall()
+        cur.close()
+        tasks = []
+        for result in results:
+            task = {
+                "id": result[0],
+                "title": result[1],
+                "description": result[2],
+                "pomodoros": result[3],
+                "taskDone": result[4],
             }
-        )
-    else:
-        return jsonify({"message": "Product not found"})
+            tasks.append(task)
+        return jsonify(tasks)
+    elif request.method == "POST":
+        print(request.json)
+        if request.json["title"] != "" or request.json["description"] != "":
+            new_task = {
+                "title": request.json["title"],
+                "description": request.json["description"],
+                "pomodoros": int(
+                    request.json["pomodoros"] if request.json["pomodoros"] != "" else 0
+                ),
+            }
+            cur = mysql.connection.cursor()
+            cur.execute(
+                "INSERT INTO tasks (title,description,pomodoros, taskDone) VALUES (%s,%s,%s,0)",
+                (
+                    new_task["title"],
+                    new_task["description"],
+                    new_task["pomodoros"],
+                ),
+            )
+            mysql.connection.commit()
+            cur.close()
+
+            return jsonify({"message": "Task added to List"})
+        else:
+            return jsonify({"message": " Task not added to list"})
+
+
+@app.route("/api/tasks/<int:id>", methods=["GET"])
+def getTask(id):
+    cur = mysql.connection.cursor()
+    cur.execute(f"select * from tasks where id = '{id}'")
+    result = cur.fetchone()
+    cur.close()
+    print(result)
+    return jsonify(result)
+
+
+# falta este
+@app.route("/api/tasks/<int:id>", methods=["PUT"])
+def editTask(id):
+    print(id)
+    print(request.json["title"])
+    cur = mysql.connection.cursor()
+    cur.execute(
+        f'update tasks set title="{request.json["title"]}", description="{request.json["description"]}", pomodoros={request.json["pomodoros"]}, taskDone={request.json["taskDone"]} where id={id}'
+    )
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({"message": "Task edited"})
+
+
+@app.route("/api/tasks/<int:id>", methods=["DELETE"])
+def deleteTask(id):
+    cur = mysql.connection.cursor()
+    cur.execute(f"delete from tasks where id ='{id}'")
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({"message": "Task deleted"})
+
+
+@app.route("/api/tasks/all", methods=["DELETE"])
+def deleteTasksAll():
+    cur = mysql.connection.cursor()
+    cur.execute("delete from tasks")
+    mysql.connection.commit()
+    cur.close()
+    return jsonify({"message": "Task deleted"})
 
 
 if __name__ == "__main__":
--- products.py
+++
@@ -1,6 +0,0 @@
-products = [
-    {"name": "laptop", "price": 800, "quantity": 4},
-    {"name": "mouse", "price": 800, "quantity": 4},
-    {"name": "monitor", "price": 800, "quantity": 4},
-    {"name": "teclado", "price": 800, "quantity": 4},
-]
